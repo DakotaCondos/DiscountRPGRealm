b7379b3553fbd2050eb3c501b7c69310f6c11efb
DakotaCondos <79940799+DakotaCondos@users.noreply.github.com>
2023-07-05 14:31:24 -0700

    Added exit button to EndGameUI

--- Assets/Scripts/EndGameUI.cs
+++ Assets/Scripts/EndGameUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGameUI : MonoBehaviour
 {
@@ -112,5 +113,10 @@ public class EndGameUI : MonoBehaviour
     {
         _playerDisplayIndex--;
         DisplayPlayer(_playerDisplayIndex);
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 }
